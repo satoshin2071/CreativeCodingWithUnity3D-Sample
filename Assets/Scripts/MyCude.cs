d927540a6045d5defe6982b0dce44bd657f84c4d
satoshin2071 <satoshin2071@users.noreply.github.com>
2016-05-12 23:07:02 +0900

    done 2 episode
    * 03 : It's Alive!,
    * 04 : Spinning on that Dizzy Edge

---
+++ Assets/Scripts/MyCude.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyCude : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+	public Vector3 spinVelocity = Vector3.zero;
+	Vector3 spinAxis = new Vector3 (0,1,0);
+
+	
+	void Start () {
+		spinVelocity = new Vector3 (Random.value, Random.value, Random.value);
+		spinAxis = Vector3.up;
+		spinAxis.x = (Random.value - Random.value) * .1f; //little bit noise spinAxis.x
+	}
+	
+	void Update () {
+		this.transform.Rotate (spinVelocity);
+		this.transform.RotateAround (Vector3.zero, spinAxis, rotateSpeed);
+	}
+	
+	public void SetSize(float size)
+	{
+		this.transform.localScale = new Vector3 (size, size, size);
+	}
+}
